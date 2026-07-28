e08065ae20abc9c20e97202e0adf04ce4ef7959e
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-28 13:01:00 +0700

    Create dashboard.js

---
+++ js/dashboard.js
@@ -0,0 +1,21 @@
+const stats={
+
+project:18,
+
+episode:421,
+
+publish:198,
+
+review:26
+
+};
+
+document.getElementById("projectCount").textContent=stats.project;
+
+document.getElementById("episodeCount").textContent=stats.episode;
+
+document.getElementById("publishCount").textContent=stats.publish;
+
+document.getElementById("reviewCount").textContent=stats.review;
+
+console.log("Dashboard Loaded");
