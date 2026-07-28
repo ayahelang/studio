d4e094e455adbeb6123d9a46e3318b7d387a1cc7
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-28 14:39:45 +0700

    Create playlists.js

---
+++ js/data/playlists.js
@@ -0,0 +1,31 @@
+export const playlists=[
+
+{
+id:1,
+name:"Web Development",
+description:"Belajar HTML, CSS, JavaScript",
+privacy:"Public"
+},
+
+{
+id:2,
+name:"Artificial Intelligence",
+description:"AI untuk Guru dan Siswa",
+privacy:"Public"
+},
+
+{
+id:3,
+name:"Shorts",
+description:"Konten pendek YouTube Shorts",
+privacy:"Public"
+},
+
+{
+id:4,
+name:"Tutorial GitHub",
+description:"Git, GitHub, GitHub Pages",
+privacy:"Public"
+}
+
+];
