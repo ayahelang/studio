546405eb7b4d97d6d7f67efab004d58590cff8a7
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-28 12:59:08 +0700

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,11 @@
+import {APP_NAME,VERSION} from "./config.js";
+
+window.addEventListener("DOMContentLoaded",()=>{
+
+document.title=APP_NAME;
+
+console.log(APP_NAME);
+
+console.log("Version",VERSION);
+
+});
