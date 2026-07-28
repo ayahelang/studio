d5a4ba4a0b9d3356b8f4d6cc6a9fee4ed08adfd1
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-28 13:52:04 +0700

    Create navbar.js

---
+++ components/navbar.js
@@ -0,0 +1,41 @@
+export function Navbar(title){
+
+return `
+
+<div class="topbar">
+
+<div>
+
+<h2>${title}</h2>
+
+<p>
+
+Collaborative YouTube Production Management
+
+</p>
+
+</div>
+
+<div class="profile">
+
+<img src="assets/avatar.png" class="avatar">
+
+<div>
+
+<b>Teddy Mulyana</b>
+
+<div class="muted">
+
+Owner
+
+</div>
+
+</div>
+
+</div>
+
+</div>
+
+`;
+
+}
