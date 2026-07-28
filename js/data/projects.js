ce30d92c96a5ca46e2a487ec47b0abe919b27cab
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-28 14:36:59 +0700

    Update and rename data/projects.js to js/data/projects.js

---
+++ js/data/projects.js
@@ -0,0 +1,59 @@
+export const projects = [
+
+{
+id:1,
+code:"HTML",
+title:"Belajar HTML Dasar",
+description:"Dasar HTML untuk pemula.",
+playlist:"Web Development",
+episodes:32,
+published:18,
+thumbnail:"assets/project/html.png",
+color:"#3b82f6",
+status:"Active",
+owner:"Teddy Mulyana"
+},
+
+{
+id:2,
+code:"CSS",
+title:"Belajar CSS",
+description:"Styling modern menggunakan CSS.",
+playlist:"Web Development",
+episodes:24,
+published:10,
+thumbnail:"assets/project/css.png",
+color:"#10b981",
+status:"Active",
+owner:"Teddy Mulyana"
+},
+
+{
+id:3,
+code:"JS",
+title:"Belajar JavaScript",
+description:"JavaScript dari dasar hingga DOM.",
+playlist:"Programming",
+episodes:42,
+published:15,
+thumbnail:"assets/project/js.png",
+color:"#f59e0b",
+status:"Planning",
+owner:"Teddy Mulyana"
+},
+
+{
+id:4,
+code:"AI",
+title:"AI Untuk Guru",
+description:"Pemanfaatan AI dalam pembelajaran.",
+playlist:"Artificial Intelligence",
+episodes:18,
+published:8,
+thumbnail:"assets/project/ai.png",
+color:"#ef4444",
+status:"Active",
+owner:"Teddy Mulyana"
+}
+
+];
