edf8d3afc9441ddbead6cfda9a3e51cc238dd2bd
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-29 13:44:42 +0700

    Update functions.sql

--- database/functions.sql
+++ database/functions.sql
@@ -3,3 +3,23 @@ create extension
 if not exists
 
 pgcrypto;
+
+create or replace function public.handle_new_user()
+returns trigger
+language plpgsql
+security definer
+as $$
+begin
+    insert into public.profiles(
+        id,
+        full_name,
+        avatar_url
+    )
+    values(
+        new.id,
+        new.raw_user_meta_data->>'full_name',
+        new.raw_user_meta_data->>'avatar_url'
+    );
+    return new;
+end;
+$$;
