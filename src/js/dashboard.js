c267ed065f29f1828a70e6af84fa1b3e4a117d43
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-29 13:25:24 +0700

    Update dashboard.js

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,24 +1,18 @@
 import AuthService from "./services/auth.service.js";
+import renderSidebar from "./layouts/sidebar.js";
+import renderNavbar from "./layouts/navbar.js";
 
-const session = await AuthService.getSession();
+const user=await AuthService.getUser();
 
-if (!session) {
-
-    location.href = "../../index.html";
-
+if(!user){
+    location.href="../../index.html";
 }
 
-const user = await AuthService.getUser();
+document.getElementById("sidebar").innerHTML=renderSidebar();
+document.getElementById("navbar").innerHTML=await renderNavbar();
 
-document.getElementById("content").innerHTML = `
-
-<h1>Halo ${user.user_metadata.full_name}</h1>
-
-<p>${user.email}</p>
-
-<img
-src="${user.user_metadata.avatar_url}"
-width="80"
-style="border-radius:50%;">
-
-`;
+document.getElementById("content").innerHTML=`
+<section class="welcome-card">
+    <h1>Halo, ${user.user_metadata.full_name} 👋</h1>
+    <p>Selamat datang di Silverhawk Studio.</p>
+</section>`;
