30b56bb3c303b652411db6f3cb114154d5a936f2
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-29 15:20:24 +0700

    Update dashboard.view.js

--- src/js/views/dashboard.view.js
+++ src/js/views/dashboard.view.js
@@ -1,13 +1,25 @@
 import statCard from "../components/stat-card.js";
 
-export function renderDashboard(stats){
+export function renderDashboard(stat){
+
     return`
-<div class="stat-grid">
-    ${statCard("Users",stats.users,"👥","#2563eb")}
-    ${statCard("Playlists",stats.playlists,"🎬","#9333ea")}
-    ${statCard("Projects",stats.projects,"📁","#16a34a")}
-    ${statCard("Episodes",stats.episodes,"🎥","#ea580c")}
-    ${statCard("Uploads",stats.uploads,"📤","#0891b2")}
-    ${statCard("Published",stats.published,"📺","#dc2626")}
-</div>`;
+
+<section class="stat-grid">
+
+${statCard("Users",stat.users,"👤")}
+
+${statCard("Teams",stat.teams,"👥")}
+
+${statCard("Playlists",stat.playlists,"📂")}
+
+${statCard("Episodes",stat.episodes,"🎬")}
+
+${statCard("Uploads",stat.uploads,"☁️")}
+
+</section>
+
+<div id="dashboardCharts"></div>
+
+`;
+
 }
