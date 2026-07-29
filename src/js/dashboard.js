b2ab1bb3031c4e8fc46bf19a0dba538ae54c100a
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-29 13:00:51 +0700

    Refactor dashboard.js for user session and display
    Refactor user session handling and update user display logic.

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,39 +1,24 @@
-import AuthService
-from "./services/auth.service.js";
+import AuthService from "./services/auth.service.js";
 
-import AuthStore
-from "./stores/auth.store.js";
+const session = await AuthService.getSession();
 
-const user=
+if (!session) {
 
-await AuthService.currentUser();
-
-if(!user){
-
-location.href="/";
+    location.href = "../../index.html";
 
 }
 
-AuthStore.setUser(user);
+const user = await AuthService.getUser();
 
-document.querySelector(
+document.getElementById("content").innerHTML = `
 
-"#content"
+<h1>Halo ${user.user_metadata.full_name}</h1>
 
-).innerHTML=`
+<p>${user.email}</p>
 
-<h1>
-
-Halo ${user.user_metadata.full_name}
-
-👋
-
-</h1>
-
-<p>
-
-${user.email}
-
-</p>
+<img
+src="${user.user_metadata.avatar_url}"
+width="80"
+style="border-radius:50%;">
 
 `;
