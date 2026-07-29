dab2c4362f87151038049c47c739e2c9993c4f74
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-29 13:45:53 +0700

    Update dashboard.js

--- src/js/dashboard.js
+++ src/js/dashboard.js
@@ -1,4 +1,5 @@
 import AuthService from "./services/auth.service.js";
+import ProfileService from "./services/profile.service.js";
 import renderSidebar from "./layouts/sidebar.js";
 import renderNavbar from "./layouts/navbar.js";
 import renderFooter from "./layouts/footer.js";
@@ -10,16 +11,16 @@ if(!user){
     location.href="../../index.html";
 }
 
-document.getElementById("sidebar").innerHTML=renderSidebar();
+const profile=await ProfileService.me();
+
+document.getElementById("sidebar").innerHTML=renderSidebar(profile);
 document.getElementById("navbar").innerHTML=await renderNavbar();
-document.getElementById("content").innerHTML=renderDashboard();
+document.getElementById("content").innerHTML=renderDashboard(profile);
 document.body.insertAdjacentHTML("beforeend",renderFooter());
 
 const logout=document.getElementById("logoutBtn");
 
-if(logout){
-    logout.addEventListener("click",async(e)=>{
-        e.preventDefault();
-        await AuthService.logout();
-    });
-}
+logout.addEventListener("click",async e=>{
+    e.preventDefault();
+    await AuthService.logout();
+});
