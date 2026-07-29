2e200a253f3bd081140e1af60ee33e01f2cb9a7d
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-29 15:35:54 +0700

    Update dashboard-page.js

--- src/js/pages/dashboard-page.js
+++ src/js/pages/dashboard-page.js
@@ -1,13 +1,20 @@
 import DashboardService from "../services/dashboard.service.js";
+import ActivityService from "../services/activity.service.js";
 import {renderDashboard} from "../views/dashboard.view.js";
+import {renderActivities} from "../views/activity.view.js";
 
 const content=document.getElementById("content");
 
 async function init(){
 
-    const stat=await DashboardService.getStatistics();
+    const [stat,activities]=await Promise.all([
+        DashboardService.getStatistics(),
+        ActivityService.latest()
+    ]);
 
-    content.innerHTML=renderDashboard(stat);
+    content.innerHTML=
+        renderDashboard(stat)+
+        renderActivities(activities);
 
 }
 
