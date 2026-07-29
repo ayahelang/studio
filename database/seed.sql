c56b9d5432d72ad18c41cc6009fcef3d42768081
Teddy Mulyana <42166005+ayahelang@users.noreply.github.com>
2026-07-29 13:37:08 +0700

    Update seed.sql

--- database/seed.sql
+++ database/seed.sql
@@ -21,3 +21,26 @@ gen_random_uuid(),
 'Owner'
 
 );
+
+
+insert into roles(name,description)
+values
+('Owner','Pemilik aplikasi'),
+('Supervisor','Pengawas tim'),
+('Editor','Editor video'),
+('Publisher','Publisher YouTube'),
+('Viewer','Hanya melihat data')
+on conflict(name) do nothing;
+
+insert into permissions(code,name)
+values
+('dashboard.view','Dashboard'),
+('project.view','Lihat Project'),
+('project.create','Buat Project'),
+('project.edit','Edit Project'),
+('episode.create','Buat Episode'),
+('episode.edit','Edit Episode'),
+('youtube.publish','Publish YouTube'),
+('team.manage','Kelola Tim')
+on conflict(code) do nothing;
+
